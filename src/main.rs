f8c79b92621b6af25d2f4f9ed0bd91b4f671c93f
Jose Armas <josearmas@cashea.app>
2025-03-27 00:34:11 -0400

    Add: health check and metrics routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use axum_prometheus::PrometheusMetricLayer;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+use crate::routes::health;
+
+mod routes;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "url_shortener=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let (prometheus_layer, metrics_handler) = PrometheusMetricLayer::pair();
+
+    let app = Router::new()
+        .route("/health", get(health))
+        .route("/metrics", get(|| async move { metrics_handler.render() }))
+        .layer(TraceLayer::new_for_http())
+        .layer(prometheus_layer);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("Could not initialize TcpListener");
+
+    tracing::debug!(
+        "listening on {}",
+        listener.local_addr().expect("Could not get local address")
+    );
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .expect("could not start server");
+
+    Ok(())
 }
